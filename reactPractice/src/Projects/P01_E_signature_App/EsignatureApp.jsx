122ad51203c79aa5d7b423d6c0e363d09130e959
Pixel4Inch <77658911+pixel4inch@users.noreply.github.com>
2025-07-17 17:14:04 +0530

    project-1

--- reactPractice/src/Projects/P01_E_signature_App/EsignatureApp.jsx
+++ reactPractice/src/Projects/P01_E_signature_App/EsignatureApp.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 import MainTitle from '../../Components/MainTitle';
 
 function EsignatureApp() {
+    const [name, setName] = React.useState('');
+    const [dob, setDob] = React.useState('');
   return (
-    <div>
-      <MainTitle titletext='P01-E-Signature App' />
-    </div>
+    <>
+      <MainTitle titletext='P01-E-Signature App'/>
+        <br/>
+         <div className='max-w-100 mx-auto px-5 py-2 bg-gray-100'>
+            <p className='font-bold py-2'>Name: {name}</p>
+             <p className='font-bold py-2'>DOB: {dob}</p>
+              <hr/> 
+        </div>
+       
+         <div className='gap-2.5 flex justify-around max-w-100 mx-auto px-5 py-2 bg-gray-100'>
+          <input type='date' className='border bg-white'value={dob} onChange={e => setDob(e.target.value)} />
+          <input type='text'className='border bg-white' value={name} onChange={e => setName(e.target.value)}/>
+        </div>
+    </>
   );
 }
 
